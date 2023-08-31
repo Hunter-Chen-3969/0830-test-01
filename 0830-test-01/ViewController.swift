499dee6caaf6fa77761979a2d3786dcccc5b470c
Hunter Chen <hunterchen3969@gmail.com>
2023-08-31 15:11:32 +0800

    comment-03

--- 0830-test-01/ViewController.swift
+++ 0830-test-01/ViewController.swift
@@ -11,12 +11,14 @@ class ViewController: UIViewController {
 
     let message = "Ｈello Git!"
     let message02 = "Ｈello Git-02"
+    let message03 = "Ｈello Git-03"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(message)
         print(message02)
+        print(message03)
     }
 
     override func didReceiveMemoryWarning(){
